34d61deb3fec855e237a419838c2e828f3488eaa
Michał Murawski <homasha@gmail.com>
2022-08-10 14:53:22 +0200

    Loop prints contents of array of type string

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,5 +1,8 @@
 public class Palindrom {
     public static void main(String[] args) {
-        
+        String[] palindrom = {"t", "e", "n", "e", "t"};
+        for (int i = 0; i < palindrom.length; i++) {
+            System.out.println(palindrom[i]);
+        }
     }
 }
