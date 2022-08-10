e4d91b7570cca035b11decd25037a91a712d5c94
Michał Murawski <homasha@gmail.com>
2022-08-10 20:29:56 +0200

    Created originalText method

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,8 +1,16 @@
 public class Palindrom {
     public static void main(String[] args) {
-        String[] palindrom = {"t", "e", "n", "e", "t"};
-        for (int i = 0; i < palindrom.length; i++) {
-            System.out.println(palindrom[i]);
+        String originalText = originalText();
+    }
+    private static String originalText() {
+        String[] palindrom = {"T", "e", "n", "e", "t"};
+        for (int i = 0; i <= palindrom.length - 1; i++) {
+            System.out.print(palindrom[i] + " | ");
+            for (int j = palindrom.length - 1; j >= 0; j--) {
+                System.out.print(palindrom[j]);
+            }
+            System.out.println();
         }
+        return null;
     }
 }
