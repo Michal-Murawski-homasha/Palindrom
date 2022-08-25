bcd73027518652abcfb5e01448e8beb00d1d4414
Michał Murawski <homasha@gmail.com>
2022-08-25 18:25:55 +0200

    Changes condition for first loop

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -4,7 +4,7 @@ public class Palindrom {
     }
     private static String originalText() {
         String[] palindrom = {"T", "e", "n", "e", "t"};
-        for (int i = 0; i <= palindrom.length - 1; i++) {
+        for (int i = 0; i < palindrom.length; i++) {
             System.out.print(palindrom[i] + " | ");
             for (int j = palindrom.length - 1; j >= 0; j--) {
                 System.out.print(palindrom[j]);
