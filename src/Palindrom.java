82eb57317e8f514c09fa63fa455f6da24e1be0cc
Michał Murawski <homasha@gmail.com>
2022-08-25 18:36:08 +0200

    Interrupts loops after printing caption once

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,15 +1,19 @@
+import java.util.Arrays;
+
 public class Palindrom {
     public static void main(String[] args) {
         String originalText = originalText();
     }
     private static String originalText() {
         String[] palindrom = {"T", "e", "n", "e", "t"};
-        for (int i = 0; i < palindrom.length; i++) {
-            System.out.print(palindrom[i] + " | ");
+        int i = 0;
+        System.out.println(Arrays.toString(palindrom));
+        for (i = 0; i < palindrom.length; i++) {
             for (int j = palindrom.length - 1; j >= 0; j--) {
                 System.out.print(palindrom[j]);
             }
             System.out.println();
+            break;
         }
         return null;
     }
